5ff1ce3f60374e271e5a61ee03cce0e312bb458b
Carlos Henrique dos Anjos Santos <104277476+carloshenriqueanjos@users.noreply.github.com>
2022-08-29 20:52:52 -0300

    Update script.js
    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btt = document.getElementById("butao");
 btt.onclick = () => {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = () => {
-        document.getElementById("demo").innerHTML = this.responseText;
+        document.getElementById("demo").innerHTML = xhttp.responseText;
     }
     xhttp.open("GET", "texto.txt", true);
     xhttp.send();
