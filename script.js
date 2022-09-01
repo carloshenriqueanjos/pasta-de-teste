98f44c1a00c706fda2ad35822526a3dd394ebdfe
Carlos Henrique dos Anjos Santos <104277476+carloshenriqueanjos@users.noreply.github.com>
2022-09-01 09:54:21 -0300

    Delete script.js
    update

--- script.js
+++
@@ -1,8 +0,0 @@
-function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        document.getElementById("demo").innerHTML = this.responseText;
-    }
-    xhttp.open("GET", "texto.txt", true);
-    xhttp.send();
-}
